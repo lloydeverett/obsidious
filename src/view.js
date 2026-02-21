e8cd09ee1edc1d846b49664f94249fa13703328c
Lloyd Everett <lloydeverett@gmail.com>
2026-02-21 15:12:28 +0200

    v0.0.2: colors, and test event listener fix

--- src/view.js
+++ src/view.js
@@ -12,8 +12,18 @@ export class ObsidiousView extends LitElement {
       }
 
       swiper-container {
-        width: 240px;
-        height: 320px;
+        width: 270px;
+        height: 360px;
+        --bg-color-1:  #960D46;
+        --bg-color-2:  #3F3F78;
+        --bg-color-3:  #98B592; --fg-color-3:  black;
+        --bg-color-4:  #F5D6BA; --fg-color-4:  black;
+        --bg-color-5:  #F49D6E; --fg-color-5:  black;
+        --bg-color-6:  #1b4b4b;
+        --bg-color-7:  #5a293c;
+        --bg-color-8:  #468189;
+        --bg-color-9:  #9DBEBB; --fg-color-9:  black;
+        --bg-color-10: #F4E9CD; --fg-color-10: black;
       }
 
       swiper-slide {
@@ -27,52 +37,73 @@ export class ObsidiousView extends LitElement {
       }
 
       swiper-slide:nth-child(1n) {
-        background-color: #F26076;
+        background-color: var(--bg-color-1);
+        color:            var(--fg-color-1, white);
       }
 
       swiper-slide:nth-child(2n) {
-        background-color: #FF9760;
+        background-color: var(--bg-color-2);
+        color:            var(--fg-color-2, white);
       }
 
       swiper-slide:nth-child(3n) {
-        background-color: #FFD150;
+        background-color: var(--bg-color-3);
+        color:            var(--fg-color-3, white);
       }
 
       swiper-slide:nth-child(4n) {
-        background-color: #458B73;
+        background-color: var(--bg-color-4);
+        color:            var(--fg-color-4, white);
       }
 
       swiper-slide:nth-child(5n) {
-        background-color: rgb(118, 163, 12);
+        background-color: var(--bg-color-5);
+        color:            var(--fg-color-5, white);
       }
 
       swiper-slide:nth-child(6n) {
-        background-color: rgb(180, 10, 47);
+        background-color: var(--bg-color-6);
+        color:            var(--fg-color-6, white);
       }
 
       swiper-slide:nth-child(7n) {
-        background-color: rgb(35, 99, 19);
+        background-color: var(--bg-color-7);
+        color:            var(--fg-color-7, white);
       }
 
       swiper-slide:nth-child(8n) {
-        background-color: rgb(0, 68, 255);
+        background-color: var(--bg-color-8);
+        color:            var(--fg-color-8, white);
       }
 
       swiper-slide:nth-child(9n) {
-        background-color: rgb(218, 12, 218);
+        background-color: var(--bg-color-9);
+        color:            var(--fg-color-9, white);
       }
 
       swiper-slide:nth-child(10n) {
-        background-color: rgb(54, 94, 77);
+        background-color: var(--bg-color-10);
+        color:            var(--fg-color-10, white);
       }
   `;
 
+  handleTouchStart(e) {
+      e.stopPropagation();
+  }
+
   render() {
     return html`
-        <swiper-container effect="cards" grab-cursor="true">
+        <swiper-container effect="cards" grab-cursor="true" @touchstart=${this.handleTouchStart}>
             <swiper-slide>Slide 1</swiper-slide>
             <swiper-slide>Slide 2</swiper-slide>
             <swiper-slide>Slide 3</swiper-slide>
+            <swiper-slide>Slide 4</swiper-slide>
+            <swiper-slide>Slide 5</swiper-slide>
+            <swiper-slide>Slide 6</swiper-slide>
+            <swiper-slide>Slide 7</swiper-slide>
+            <swiper-slide>Slide 8</swiper-slide>
+            <swiper-slide>Slide 9</swiper-slide>
+            <swiper-slide>Slide 10</swiper-slide>
         </swiper-container>
     `;
   }
